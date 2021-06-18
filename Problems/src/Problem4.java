2fc97c9fc2fb85d06416d04f419b25e22fdeaec5
B150910045 <70797128+B150910045@users.noreply.github.com>
2021-06-18 08:08:57 +0800

    Bodlogo 4

--- Problems/src/Problem4.java
+++ Problems/src/Problem4.java
@@ -1,4 +1,10 @@
 
 public class Problem4 {
-
+	Boolean usuh_eseh(int[] array, int hemjee) {
+		for(int i = 0; i<hemjee - 1; i++) {
+			if(array[i] > array[i + 1])
+				return false;
+		}
+		return true;
+	}
 }
--- Problems/src/Problem4Window.java
+++ Problems/src/Problem4Window.java
@@ -1,10 +1,18 @@
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Problem4Window {
 
 	private JFrame frame;
+	private JTextField hemjeeText;
+	private JTextField arrayText;
 
 	/**
 	 * Launch the application.
@@ -33,9 +41,45 @@ public class Problem4Window {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		Problem4 prob = new Problem4();
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Arithmetic dundaj oloh");
+		frame.getContentPane().setLayout(null);
+		
+		hemjeeText = new JTextField();
+		hemjeeText.setBounds(10, 10, 150, 40);
+		hemjeeText.setFont(new Font("Tahoma", Font.BOLD, 18));
+		frame.getContentPane().add(hemjeeText);
+		hemjeeText.setColumns(10);
+		
+		arrayText = new JTextField();
+		arrayText.setBounds(180, 10, 150, 40);
+		arrayText.setFont(new Font("Tahoma", Font.BOLD, 18));
+		frame.getContentPane().add(arrayText);
+		arrayText.setColumns(10);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(10, 140, 160, 40);
+		frame.getContentPane().add(textArea);
+		
+		JButton add_btn = new JButton("Usuh Eseh");
+		add_btn.setBounds(10, 55, 160, 40);
+		frame.getContentPane().add(add_btn);
+		add_btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int hemjee = Integer.parseInt(hemjeeText.getText());
+				int[] array = new int[hemjee];
+				String string = arrayText.getText();
+				String[] arrayString = string.split(" ");
+				
+				for(int i=0; i<hemjee ; i++)
+					array[i] = Integer.parseInt(arrayString[i]);
+				textArea.setText("hariu: " + prob.usuh_eseh(array, hemjee));
+			}
+		});
 	}
 
 }
