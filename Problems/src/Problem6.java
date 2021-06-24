60aae367ed658daaa118703498b42d0a6e2a623d
B150910045 <70797128+B150910045@users.noreply.github.com>
2021-06-24 20:27:40 +0800

    bodlogo 7

--- Problems/src/Problem6.java
+++ Problems/src/Problem6.java
@@ -16,7 +16,12 @@ public class Problem6 {
 		Scanner scn = new Scanner(System.in);
 		int[] toonuud = new int[n];
 
+		System.out.println("\r\n"
+				+ "RGB7114 - Тэгш тоонууд YES\r\n"
+				+ "Гурван тоо өгөгдөнө. Тоо тэгш бол YES үгүй бол NO гэж хэвлэ.");
+		
 		for(int i = 0; i < n; ++i) {
+			System.out.print("too " + (i+1) + " :");
 			toonuud[i] = scn.nextInt();
 		}
 		
--- Problems/src/Problem7.java
+++ Problems/src/Problem7.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
 
 public class Problem7 {
+	public static int function(int too) {
+		return 3 * too - 5;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int too;
 
+		System.out.println("\r\n"
+				+ "RGB7005 - Функц 1\r\n"
+				+ "y=3x-5");
+		
+		System.out.print("too:");
+		too = scn.nextInt();		
+		scn.close();
+		System.out.println("hariu: " + function(too));
+	}
 }
+
