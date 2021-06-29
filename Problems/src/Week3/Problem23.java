b57e6ac659e24445277d843c036eed3e0d80b968
B150910045 <70797128+B150910045@users.noreply.github.com>
2021-06-29 12:15:59 +0800

    Bodlogo 23

--- Problems/src/Week3/Problem23.java
+++ Problems/src/Week3/Problem23.java
@@ -1,5 +1,17 @@
 package Week3;
 
 public class Problem23 {
-
+	public int function(int too) {
+		int sond, hariu = 1;
+		if(too % 2 == 1)
+			sond = 1;
+		else 
+			sond = 2;
+		
+		for(int i = sond; i <= too; i = i + 2) {
+			hariu = hariu * i;
+		}
+		
+		return hariu;
+	}
 }
